a6867c11da995ded0cb9530afdd4770f859c7f0f
Sam Martin <sam.martin@7digital.com>
2018-05-14 14:40:29 +0100

    Added option to change ALL site's limit option

--- attributes/default.rb
+++ attributes/default.rb
@@ -161,4 +161,8 @@ default['proftpd-ii']['tls_ciphers'] = 'ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM
 # use an user authorized keys file to permit users to login using keys
 default['proftpd-ii']['sftp_userauthorizedkeys'] = nil
 
+# What keys will the host use to identify itself via sftp?
 default['proftpd-ii']['sftp_hostkeys'] = ['/etc/ssh/ssh_host_rsa_key']
+
+# What do you want to allow to access the default site?
+default['proftpd-ii']['all_site_limit'] = 'AllowAll'
